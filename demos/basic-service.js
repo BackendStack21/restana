b8b29a44439815ac54880b7a3c90d60e37b81d8d
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-04-28 21:25:24 +0200

    adding example demo

---
+++ demos/basic-service.js
@@ -0,0 +1,20 @@
+const service = require('./../index')({});
+
+// custom middleware to attach the X-Response-Time header to the response
+service.use((req, res, next) => {
+    let now = new Date().getTime();
+
+    res.on('response', data => {
+        data.res.setHeader('X-Response-Time', new Date().getTime() - now);
+    });
+
+    return next();
+});
+
+// the /v1/welcome route handler
+service.get('/v1/welcome', (req, res) => {
+    res.send('Hello World!');
+});
+
+// start the server
+service.start();
