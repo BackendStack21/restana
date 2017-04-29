652916e8e6f795ed3609676c62d39e17c0ed7ef6
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-04-29 16:15:36 +0200

    adding morgan middleware example

---
+++ demos/morgan.js
@@ -0,0 +1,10 @@
+const service = require('./../index')({});
+const morgan = require('morgan');
+
+service.use(morgan('tiny'));
+service.get('/v1/welcome', (req, res) => {
+    res.send('Hello World!');
+});
+
+// start the server
+service.start();
