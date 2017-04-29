17f6cddbf4fcc77b3e0208a75b94a48c174f8a22
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-04-29 16:14:38 +0200

    removing callback usage

--- libs/middleware-chain.js
+++ libs/middleware-chain.js
@@ -1,13 +1,13 @@
-const next = (middlewares, req, res, cb) => {
+const next = (middlewares, req, res) => {
     let middleware = middlewares.shift();
 
     return (err) => {
         if (err) return res.send(err);
         if (res.statusCode == 200 && !res.finished) {
-            if (!middleware) return cb();
+            if (!middleware) return;
             else {
                 try {
-                    let result = middleware.handler.call(middleware.context, req, res, next(middlewares, req, res, cb));
+                    let result = middleware.handler.call(middleware.context, req, res, next(middlewares, req, res));
                     if (result instanceof Promise) { // async support
                         result.catch(res.send);
                     }
