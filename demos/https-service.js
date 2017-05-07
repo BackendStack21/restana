2d72725902f48277809cacdff637b74890f09561
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-05-07 12:56:59 +0200

    adding HTTPS service example

---
+++ demos/https-service.js
@@ -0,0 +1,20 @@
+const https = require('https');
+const pem = require('pem');
+
+pem.createCertificate({
+    days: 1,
+    selfSigned: true
+}, (err, keys) => {
+    const service = require('./../index')({
+        server: https.createServer({
+            key: keys.serviceKey,
+            cert: keys.certificate
+        })
+    });
+
+    service.get('/v1/welcome', (req, res) => {
+        res.send('Hello World!');
+    });
+
+    service.start();
+});
