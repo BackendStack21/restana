83708d82236ea3dea79359369ad058bd6216fb51
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2017-09-26 18:24:22 +0200

    adding headers friendly api

--- libs/response-extensions.js
+++ libs/response-extensions.js
@@ -1,40 +1,42 @@
 module.exports.send = (req, res) => (data = 200, code = 200, headers = {}) => {
-    res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('content-type', 'text/plain');
+  Object.keys(headers).forEach((key) => {
+    res.setHeader(key.toLowerCase(), headers[key]);
+  });
 
-    return new Promise(async(resolve, reject) => {
-        if (data instanceof Promise)
-            data = await data;
+  return new Promise(async (resolve, reject) => {
+    if (data instanceof Promise) data = await data;
 
-        if (data instanceof Error) {
-            code = data.status || data.code || 500;
-            data = {
-                code,
-                message: data.message,
-                data: data.data
-            };
-        }
-        if ('number' == typeof data) {
-            code = parseInt(data);
-            data = res.body;
-        }
-        if ('object' == typeof data) {
-            res.setHeader('Content-Type', 'application/json');
-            data = JSON.stringify(data);
-        }
+    if (data instanceof Error) {
+      code = data.status || data.code || 500;
+      data = {
+        code,
+        message: data.message,
+        data: data.data
+      };
+    }
+    if (typeof data === 'number') {
+      code = parseInt(data, 10);
+      data = res.body;
+    }
+    if (typeof data === 'object') {
+      res.setHeader('content-type', 'application/json');
+      data = JSON.stringify(data);
+    }
 
-        // emit response event before send and terminate
-        let params = {
-            res,
-            req,
-            data,
-            code
-        }
-        res.emit('response', params);
+    // emit response event before send and terminate
+    const params = {
+      res,
+      req,
+      data,
+      code
+    };
+    res.emit('response', params);
 
-        res.writeHead(code);
-        res.end(params.data, 'utf-8', (err) => {
-            if (err) reject(err);
-            resolve();
-        });
+    res.writeHead(code);
+    res.end(params.data, 'utf-8', (err) => {
+      if (err) reject(err);
+      resolve();
     });
+  });
 };
