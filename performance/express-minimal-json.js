ce270a1cfc503c30c9e24bf5ef1019d413fbce53
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2018-05-27 22:01:05 +0200

    adding express comparison demos

---
+++ performance/express-minimal-json.js
@@ -0,0 +1,8 @@
+const service = require('express')({})
+
+service.get('/hi', (req, res) => {
+  res.send({
+    msg: 'Hello World!'
+  })
+})
+service.listen(3000)
---
+++ performance/express-minimal.js
@@ -0,0 +1,6 @@
+const service = require('express')({})
+
+service.get('/hi', (req, res) => {
+  res.send('Hello World!')
+})
+service.listen(3000)
