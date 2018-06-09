5db84eae29e1f3d80d4d91a619c69fb45c83f4cf
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2018-06-09 22:24:06 +0200

    removing Promise resolution on send method

--- libs/response-extensions.js
+++ libs/response-extensions.js
@@ -1,43 +1,36 @@
-module.exports.send = (req, res) => (data = 200, code = 200, headers = {}) => {
+module.exports.send = (req, res) => (data = 200, code = 200, headers = {}, cb = () => {}) => {
   res.setHeader('content-type', 'text/plain')
   Object.keys(headers).forEach((key) => {
     res.setHeader(key.toLowerCase(), headers[key])
   })
 
-  return new Promise(async (resolve, reject) => {
-    if (data instanceof Promise) data = await data
-
-    if (data instanceof Error) {
-      code = data.status || data.code || 500
-      data = {
-        errClass: data.constructor.name,
-        code,
-        message: data.message,
-        data: data.data
-      }
-    } else if (typeof data === 'number') {
-      code = parseInt(data, 10)
-      data = res.body
+  if (data instanceof Error) {
+    code = data.status || data.code || 500
+    data = {
+      errClass: data.constructor.name,
+      code,
+      message: data.message,
+      data: data.data
     }
+  } else if (typeof data === 'number') {
+    code = parseInt(data, 10)
+    data = res.body
+  }
 
-    // emit response event
-    const params = {
-      res,
-      req,
-      data,
-      code
-    }
-    res.emit('response', params)
+  // emit response event
+  const params = {
+    res,
+    req,
+    data,
+    code
+  }
+  res.emit('response', params)
 
-    if (typeof data === 'object') {
-      res.setHeader('content-type', 'application/json')
-      params.data = JSON.stringify(params.data)
-    }
+  if (typeof data === 'object') {
+    res.setHeader('content-type', 'application/json')
+    params.data = JSON.stringify(params.data)
+  }
 
-    res.writeHead(params.code)
-    res.end(params.data, (err) => {
-      if (err) reject(err)
-      resolve()
-    })
-  })
+  res.writeHead(params.code)
+  res.end(params.data, cb)
 }
