e14037270149a1ff3fedbe270bfab50ec87256aa
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2018-10-10 23:15:02 +0200

    adding response-time demo

---
+++ demos/response-time.js
@@ -0,0 +1,12 @@
+const service = require('./../index')({})
+
+// custom middleware to attach the X-Response-Time header to the response
+service.use(require('response-time')())
+
+// the /v1/welcome route handler
+service.get('/v1/welcome', (req, res) => {
+  res.send('Hello World!')
+})
+
+// start the server
+service.start()
