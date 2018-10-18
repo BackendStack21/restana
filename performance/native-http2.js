90ae466858b50ba16a80abd239a32f40ebb58cf3
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2018-10-18 16:54:08 +0200

    adding inline comments

--- performance/native-http2.js
+++ performance/native-http2.js
@@ -1,6 +1,8 @@
 const http = require('http2')
 
 const service = http.createServer()
+
+// backward API compatibility
 service.on('request', (req, res) => {
   if (req.method === 'GET' && req.url === '/hi') {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
---
+++ performance/native-http3.js
@@ -0,0 +1,33 @@
+const http2 = require('http2')
+const pem = require('pem')
+
+pem.createCertificate({
+  days: 1,
+  selfSigned: true
+}, (err, keys) => {
+  if (err) console.error(err)
+
+  const service = http2.createSecureServer({
+    key: keys.serviceKey,
+    cert: keys.certificate
+  })
+
+  // streams API example
+  service.on('stream', (stream, headers) => {
+    if (headers[':path'] === '/hi' && headers[':method'] === 'GET') {
+      stream.respond({
+        'content-type': 'text/html',
+        ':status': 200
+      })
+
+      stream.end('Hello World!')
+    } else {
+      stream.respond({
+        ':status': 404
+      })
+      stream.end()
+    }
+  })
+
+  service.listen(3000)
+})
