8beb1b7da9c4706eeca3c47df6496117fc2db827
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-01-03 16:28:59 +0100

    adding anumargak router performance test script

---
+++ performance/restana-minimal-cluster-anumargak.js
@@ -0,0 +1,26 @@
+const anumargak = require('anumargak')
+const cluster = require('cluster')
+
+if (cluster.isMaster) {
+  const cpus = require('os').cpus().length
+
+  for (var i = 0; i < cpus; i += 1) {
+    cluster.fork()
+  }
+} else {
+  const service = require('../index')({
+    routerFactory: (options) => {
+      return anumargak(options)
+    }
+  })
+
+  service.get('/hi', (req, res) => {
+    res.send('Hello World!')
+  })
+  service.start()
+}
+
+cluster.on('exit', function (worker) {
+  console.log('Worker %d died :(', worker.id)
+  cluster.fork()
+})
