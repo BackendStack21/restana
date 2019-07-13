adf3c34b7ea6d0df38c9671ee314769d0e8aed47
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-07-13 09:47:36 +0200

    adding vertx.js performance script

---
+++ performance/vertx-minimal.js
@@ -0,0 +1,9 @@
+/* global vertx */
+var Router = require('vertx-web-js/router')
+var router = Router.router(vertx)
+
+router.route('/hi').handler(function (routingContext) {
+  routingContext.response().putHeader('content-type', 'text/html').end('Hello World!')
+})
+
+vertx.createHttpServer().requestHandler(router.handle).listen(3000)
