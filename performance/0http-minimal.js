cd6c9801c9a05b6625655e2eaca7619de3201bb8
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-07-22 15:57:31 +0200

    updating 0http scripts

--- performance/0http-minimal.js
+++ performance/0http-minimal.js
@@ -1,9 +1,24 @@
 const cero = require('0http')
+const low = require('0http/lib/server/low')
 
-const { router, server } = cero()
+const { router, server } = cero({
+  server: low()
+})
+
+router.on('GET', '/', (req, res) => {
+  res.end()
+})
 
 router.on('GET', '/hi', (req, res) => {
   res.end('Hello World!')
 })
 
-server.listen(3000)
+router.on('GET', '/user/:id', (req, res, params) => {
+  res.end(params.id)
+})
+
+router.on('POST', '/user', (req, res) => {
+  res.end()
+})
+
+server.listen(3000, socket => {})
