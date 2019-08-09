f92eed80d658f2bbb0cf4cd765b5fdfb9ad4558d
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-08-09 13:29:39 +0200

    updating native example

--- performance/native-http.js
+++ performance/native-http.js
@@ -3,7 +3,6 @@ const http = require('http')
 const service = new http.Server()
 service.on('request', (req, res) => {
   if (req.method === 'GET' && req.url === '/hi') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Hello World!')
   } else {
     res.statusCode = 404
