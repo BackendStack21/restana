4654e016da2eaa726f8a2ed1f4a21877a7e26966
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-08-27 20:51:36 +0200

    linting

--- performance/express-payload.js
+++ performance/express-payload.js
@@ -4,7 +4,7 @@ service.get('/:name/:age', (req, res) => {
   const { name, age } = req.params
   res.send({
     msg: `Dear ${name}, you still can learn at your ${age}s ` +
-      `that express is !awesome ;)`,
+      'that express is !awesome ;)',
     name,
     age,
     numbers: [...Array(1000).keys()]
