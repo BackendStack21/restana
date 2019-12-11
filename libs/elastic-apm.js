211abdf886af1d32a34b3cd0ea10a73b9260c354
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-12-11 16:34:45 +0100

    adding elastic apm integration

---
+++ libs/elastic-apm.js
@@ -0,0 +1,22 @@
+const methods = require('./methods')
+
+module.exports = ({ apm }) => {
+  return {
+    patch (app) {
+      methods.forEach(method => {
+        const ref = app[method]
+
+        app[method] = (path, ...args) => {
+          args.unshift((req, res, next) => {
+            // instrumenting APM transaction name
+            apm.setTransactionName(path)
+
+            return next()
+          })
+
+          return ref(path, args)
+        }
+      })
+    }
+  }
+}
