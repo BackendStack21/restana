4e7dd373a889a6861e3aa7b38f4a9d7467c198a3
Rolando Santamaria Maso <kyberneees@gmail.com>
2020-02-18 16:14:22 +0100

    updating swagger demo to use restana's routers

--- demos/swagger/controllers/Helloer.js
+++ demos/swagger/controllers/Helloer.js
@@ -1,6 +1,10 @@
-module.exports.sayHi = (req, res) => {
-  res.send({
-    name: req.swagger.params.name.value,
-    format: req.swagger.params.format.value
+module.exports = (router) => {
+  router.get('/sayHi/:name', (req, res) => {
+    res.send({
+      name: req.swagger.params.name.value,
+      format: req.swagger.params.format.value
+    })
   })
+
+  return router
 }
--- demos/swagger/index.js
+++ demos/swagger/index.js
@@ -1,15 +1,14 @@
-const path = require('path')
 const service = require('./../../index')({})
 const swagger = require('swagger-tools')
 const spec = require('./swagger.json')
+const helloerController = require('./controllers/Helloer')
 
 swagger.initializeMiddleware(spec, async (middleware) => {
   service.use(middleware.swaggerMetadata())
   service.use(middleware.swaggerValidator())
   service.use(middleware.swaggerUi())
-  service.use(middleware.swaggerRouter({
-    controllers: path.join(__dirname, '/controllers')
-  }))
+
+  service.use('/api', helloerController(service.newRouter()))
 
   await service.start(8000)
   console.log('API documentation is now running at http://localhost:8000/docs/')
