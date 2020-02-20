c12d1f520a4a4a97c8293be6c5d8750de668929c
Rolando Santamaria Maso <kyberneees@gmail.com>
2020-02-20 17:28:05 +0100

    updating error-handler demo

--- demos/error-handler.js
+++ demos/error-handler.js
@@ -5,12 +5,20 @@ const service = require('../index')({
   }
 })
 
+service.use(async (req, res, next) => {
+  try {
+    await next()
+  } catch (err) {
+    return next(err)
+  }
+})
+
 service.get('/throw', (req, res) => {
   throw new Error('Upps!')
 })
 
 const router = service.newRouter()
-router.get('/throw', (req, res) => {
+router.get('/throw', async (req, res) => {
   throw new Error('Upps from nested router!')
 })
 
