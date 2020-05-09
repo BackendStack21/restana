9448564ddd1947a3bc96f78157251748f9c945e9
Rolando Santamaria Maso <kyberneees@gmail.com>
2020-05-09 11:08:58 +0200

    removing unrequired middleware

--- demos/error-handler.js
+++ demos/error-handler.js
@@ -7,14 +7,6 @@ const service = require('../index')({
   }
 })
 
-service.use(async (req, res, next) => {
-  try {
-    await next()
-  } catch (err) {
-    return next(err)
-  }
-})
-
 service.get('/throw', (req, res) => {
   throw new Error('Upps!')
 })
