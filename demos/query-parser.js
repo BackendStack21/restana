dfe87d1b2fae0d550bffded3792ba81826d55d76
Rolando Santamaria Maso <kyberneees@Rolandos-MBP.fritz.box>
2020-05-24 09:04:37 +0200

    updating query parser demo

--- demos/query-parser.js
+++ demos/query-parser.js
@@ -1,9 +1,6 @@
 'use strict'
 
-const queryParser = require('connect-query')
 const service = require('./../index')({})
-service.use(queryParser())
-
 service.get('/params', (req, res) => {
   res.send(req.query)
 })
