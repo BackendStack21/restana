74f6a10932701d0b31d5c32b607c44b8cbacdbb9
Rolando Santamaria Maso <kyberneees@gmail.com>
2020-05-26 09:19:22 +0200

    improve perf + source code refactoring

--- libs/response-extensions.js
+++ libs/response-extensions.js
@@ -1,40 +1,36 @@
 'use strict'
 
-const { forEach } = require('./utils')
+const { forEachObject } = require('./utils')
 
 const CONTENT_TYPE_HEADER = 'content-type'
-const CONTENT_TYPE = {
-  JSON: 'application/json; charset=utf-8',
-  PLAIN: 'text/plain; charset=utf-8',
-  OCTET: 'application/octet-stream'
-}
+const TYPE_JSON = 'application/json; charset=utf-8'
+const TYPE_PLAIN = 'text/plain; charset=utf-8'
+const TYPE_OCTET = 'application/octet-stream'
+
 const NOOP = () => {}
 
-const setHeader = (res, value) => {
-  if (!res.hasHeader(CONTENT_TYPE_HEADER)) {
-    res.setHeader(CONTENT_TYPE_HEADER, value)
-  }
-}
-
 const stringify = (obj) => {
-  // ToDo: fast json stringify
+  // ToDo: fast json stringify ?
   return JSON.stringify(obj)
 }
 
-const sendJSON = (res, obj, cb) => {
-  res.end(stringify(obj), cb)
+const preEnd = (res, contentType, statusCode) => {
+  res.setHeader(CONTENT_TYPE_HEADER, contentType || TYPE_PLAIN)
+  res.statusCode = statusCode
 }
 
-const sendError = (res, error, cb) => {
+const parseErr = (error) => {
   const errorCode = error.status || error.code || error.statusCode
-  res.statusCode = typeof errorCode === 'number' ? parseInt(errorCode) : 500
-  res.setHeader(CONTENT_TYPE_HEADER, CONTENT_TYPE.JSON)
+  const statusCode = typeof errorCode === 'number' ? parseInt(errorCode) : 500
 
-  return sendJSON(res, {
-    code: res.statusCode,
-    message: error.message,
-    data: error.data
-  }, cb)
+  return [
+    statusCode,
+    stringify({
+      code: statusCode,
+      message: error.message,
+      data: error.data
+    })
+  ]
 }
 
 /**
@@ -43,42 +39,49 @@ const sendError = (res, error, cb) => {
  */
 module.exports.send = (options, req, res) => {
   return (data = 200, code = 200, headers = null, cb = NOOP) => {
-    if (headers !== null) {
-      forEach(headers, (value, key) => {
-        res.setHeader(key.toLowerCase(), value)
-      })
-    }
+    let contentType
 
     if (data instanceof Error) {
-      sendError(res, data, cb)
-      return
-    }
+      const [statusCode, errStr] = parseErr(data)
+      contentType = TYPE_JSON
+      code = statusCode
+      data = errStr
+    } else {
+      if (headers && typeof headers === 'object') {
+        forEachObject(headers, (value, key) => {
+          res.setHeader(key.toLowerCase(), value)
+        })
+      }
 
-    if (typeof data === 'number') {
-      code = parseInt(data, 10)
-      data = res.body
-    }
+      // NOTE: only retrieve content-type after setting custom headers
+      contentType = res.getHeader(CONTENT_TYPE_HEADER)
 
-    res.statusCode = code
+      if (typeof data === 'number') {
+        code = parseInt(data, 10)
+        data = res.body
+      }
 
-    if (data != null) {
-      if (typeof data === 'string') {
-        setHeader(res, CONTENT_TYPE.PLAIN)
-      } else if (data instanceof Buffer) {
-        setHeader(res, CONTENT_TYPE.OCTET)
-      } else if (typeof data.pipe === 'function') {
-        setHeader(res, CONTENT_TYPE.OCTET)
-        data.pipe(res)
-        data.on('end', cb)
-        return
-      } else if (typeof data === 'object') {
-        setHeader(res, CONTENT_TYPE.JSON)
-        sendJSON(res, data, cb)
-        return
+      if (data) {
+        if (data instanceof Buffer) {
+          contentType = contentType || TYPE_OCTET
+        } else if (!!data && typeof data.pipe === 'function') {
+          contentType = contentType || TYPE_OCTET
+
+          // NOTE: we exceptionally handle the response termination for streams
+          preEnd(res, contentType, code)
+
+          data.pipe(res)
+          data.on('end', cb)
+
+          return
+        } else if (typeof data === 'object') {
+          contentType = contentType || TYPE_JSON
+          data = stringify(data)
+        }
       }
     }
 
-    // finally end request
+    preEnd(res, contentType, code)
     res.end(data, cb)
   }
 }
