7ea33c71eddc1c8948ee71438c44f6873d91fb67
Rolando Santamaria Maso <kyberneees@gmail.com>
2020-05-27 12:01:10 +0200

    adding "or vs if" perf bench

---
+++ performance/suites/or-vs-if.js
@@ -0,0 +1,36 @@
+/**
+ * Benchmark to monitor performance regresions on the `res.send` method
+ */
+const Benchmark = require('benchmark')
+const suite = new Benchmark.Suite()
+
+let contentType
+const headerExists = true
+const value = 'text/plain'
+
+// add tests
+suite
+  .add('or', function () {
+    contentType = contentType || value
+  })
+  .add('or is set', function () {
+    contentType = true || value
+  })
+  .add('if', function () {
+    if (!contentType) contentType = value
+  })
+  .add('if exists', function () {
+    if (!headerExists) contentType = value
+  })
+  .on('complete', function () {
+    console.log(this.filter('successful').sort((a, b) => {
+      a = a.stats; b = b.stats
+      return (a.mean + a.moe > b.mean + b.moe ? 1 : -1)
+    }).map(suite => {
+      return {
+        name: suite.name,
+        mean: suite.stats.mean.toFixed(10)
+      }
+    }))
+  })
+  .run({ async: false })
