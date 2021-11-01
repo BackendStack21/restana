36fe3ec84b1738594ff668317e8b1447f9b210d3
Rolando Santamaria Maso <kyberneees@gmail.com>
2021-11-01 15:45:42 +0100

    adding native restana swagger validator demo

---
+++ demos/openapi/index.js
@@ -0,0 +1,36 @@
+const restana = require('../../index')
+const path = require('path')
+
+const {
+  SwaggerValidationError,
+  SwaggerValidator
+} = require('restana-swagger-validator')
+
+const app = restana({
+  errorHandler: (err, req, res) => {
+    if (err instanceof SwaggerValidationError) {
+      res.statusCode = err.statusCode
+      res.send({
+        message: err.message,
+        errors: err.errors
+      })
+    } else {
+      res.send(err)
+    }
+  }
+})
+
+SwaggerValidator(app, path.join(__dirname, '/spec.json'), {
+  buildResponses: true,
+  publicApiEndpoint: 'http://localhost:3000'
+})
+
+app.get('/pets', (req, res) => {
+  res.send([{
+    id: 1,
+    name: 'Pluto',
+    tag: 'dog'
+  }])
+})
+
+app.start()
